5f6bb862243f86b952a032dc3d2b6d9eb607b3c8
Carla Pezzone <carlapezzone@gmail.com>
2023-08-29 17:57:04 -0300

    actualizacion utils

--- utils.py
+++ utils.py
@@ -1,11 +1,25 @@
 import pandas as pd
 from textblob import TextBlob
+import re
 
 def verificar_tipo_datos(df):
     '''
-    Verifica el tipo de dato contenido en cada columna de un dataframe.
-    Tiene como parámetro el dataframe a evaluar y devuelve un resumen de el/los tipos de datos, 
-    porcentaje de nulos y no nulos y cantidad de nulos por cada columna.
+    Realiza un análisis de los tipos de datos y la presencia de valores nulos en un DataFrame.
+
+    Esta función toma un DataFrame como entrada y devuelve un resumen que incluye información sobre
+    los tipos de datos en cada columna, el porcentaje de valores no nulos y nulos, así como la
+    cantidad de valores nulos por columna.
+
+    Parameters:
+        df (pandas.DataFrame): El DataFrame que se va a analizar.
+
+    Returns:
+        pandas.DataFrame: Un DataFrame que contiene el resumen de cada columna, incluyendo:
+        - 'nombre_campo': Nombre de cada columna.
+        - 'tipo_datos': Tipos de datos únicos presentes en cada columna.
+        - 'no_nulos_%': Porcentaje de valores no nulos en cada columna.
+        - 'nulos_%': Porcentaje de valores nulos en cada columna.
+        - 'nulos': Cantidad de valores nulos en cada columna.
     '''
 
     mi_dict = {"nombre_campo": [], "tipo_datos": [], "no_nulos_%": [], "nulos_%": [], "nulos": []}
@@ -22,14 +36,134 @@ def verificar_tipo_datos(df):
         
     return df_info
 
-def analyze_sentiment(review):
+def analisis_sentimiento(review):
+    '''
+    Realiza un análisis de sentimiento en un texto dado y devuelve un valor numérico que representa el sentimiento.
+
+    Esta función utiliza la librería TextBlob para analizar el sentimiento en un texto dado y
+    asigna un valor numérico de acuerdo a la polaridad del sentimiento.
+
+    Parameters:
+        review (str): El texto que se va a analizar para determinar su sentimiento.
+
+    Returns:
+        int: Un valor numérico que representa el sentimiento del texto:
+             - 0 para sentimiento negativo.
+             - 1 para sentimiento neutral o no clasificable.
+             - 2 para sentimiento positivo.
+    '''
     if review is None:
-        return 1  # Neutral if review is None
+        return 1
     analysis = TextBlob(review)
     polarity = analysis.sentiment.polarity
-    if polarity < -0.2:  # Para negativos
-        return 0  # Malo si el sentimiento es negativo
-    elif polarity > 0.2:  # Para positivos
-        return 2  # Positivo si el sentimiento es positivo
+    if polarity < -0.2:
+        return 0  
+    elif polarity > 0.2: 
+        return 2 
     else:
-        return 1  # Neutral en otros casos
+        return 1 
+    
+def ejemplos_review_por_sentimiento(reviews, sentiments):
+    '''
+    Imprime ejemplos de reviews para cada categoría de análisis de sentimiento.
+
+    Esta función recibe dos listas paralelas, `reviews` que contiene los textos de las reviews
+    y `sentiments` que contiene los valores de sentimiento correspondientes a cada review.
+    
+    Parameters:
+        reviews (list): Una lista de strings que representan los textos de las reviews.
+        sentiments (list): Una lista de enteros que representan los valores de sentimiento
+                          asociados a cada review (0, 1, o 2).
+
+    Returns:
+        None: La función imprime los ejemplos de reviews para cada categoría de sentimiento.
+    '''
+    for sentiment_value in range(3):
+        print(f"Para la categoría de análisis de sentimiento {sentiment_value} se tienen estos ejemplos de reviews:")
+        sentiment_reviews = [review for review, sentiment in zip(reviews, sentiments) if sentiment == sentiment_value]
+        
+        for i, review in enumerate(sentiment_reviews[:3], start=1):
+            print(f"Review {i}: {review}")
+        
+        print("\n")
+
+def verifica_duplicados_por_columna(df, columna):
+    '''
+    Verifica y muestra filas duplicadas en un DataFrame basado en una columna específica.
+
+    Esta función toma como entrada un DataFrame y el nombre de una columna específica.
+    Luego, identifica las filas duplicadas basadas en el contenido de la columna especificada,
+    las filtra y las ordena para una comparación más sencilla.
+
+    Parameters:
+        df (pandas.DataFrame): El DataFrame en el que se buscarán filas duplicadas.
+        columna (str): El nombre de la columna basada en la cual se verificarán las duplicaciones.
+
+    Returns:
+        pandas.DataFrame or str: Un DataFrame que contiene las filas duplicadas filtradas y ordenadas,
+        listas para su inspección y comparación, o el mensaje "No hay duplicados" si no se encuentran duplicados.
+    '''
+    # Se filtran las filas duplicadas
+    duplicated_rows = df[df.duplicated(subset=columna, keep=False)]
+    if duplicated_rows.empty:
+        return "No hay duplicados"
+    
+    # se ordenan las filas duplicadas para comparar entre sí
+    duplicated_rows_sorted = duplicated_rows.sort_values(by=columna)
+    return duplicated_rows_sorted
+
+def obtener_anio_release(fecha):
+    '''
+    Extrae el año de una fecha en formato 'yyyy-mm-dd' y maneja valores nulos.
+
+    Esta función toma como entrada una fecha en formato 'yyyy-mm-dd' y devuelve el año de la fecha si
+    el dato es válido. Si la fecha es nula o inconsistente, devuelve 'Dato no disponible'.
+
+    Parameters:
+        fecha (str or float or None): La fecha en formato 'yyyy-mm-dd'.
+
+    Returns:
+        str: El año de la fecha si es válido, 'Dato no disponible' si es nula o el formato es incorrecto.
+    '''
+    if pd.notna(fecha):
+        try:
+            return fecha.split('-')[0]
+        except:
+            return 'Dato no disponible'
+    else:
+        return 'Dato no disponible'
+    
+def reemplaza_a_flotante(value):
+    '''
+    Reemplaza valores no numéricos y nulos en una columna con 0.0.
+
+    Esta función toma un valor como entrada y trata de convertirlo a un número float.
+    Si la conversión es exitosa, el valor numérico se mantiene. Si la conversión falla o
+    el valor es nulo, se devuelve 0.0 en su lugar.
+
+    Parameters:
+        value: El valor que se va a intentar convertir a un número float o nulo.
+
+    Returns:
+        float: El valor numérico si la conversión es exitosa o nulo, o 0.0 si la conversión falla.
+    '''
+    if pd.isna(value):
+        return 0.0
+    try:
+        float_value = float(value)
+        return float_value
+    except:
+        return 0.0
+    
+def convertir_fecha(cadena_fecha):
+    
+    match = re.search(r'(\w+\s\d{1,2},\s\d{4})', cadena_fecha)
+    if match:
+        fecha_str = match.group(1)
+        try:
+            fecha_dt = pd.to_datetime(fecha_str)
+            return fecha_dt.strftime('%Y-%m-%d')
+        except:
+            return 'Fecha inválida'
+    else:
+        return 'Formato inválido'
